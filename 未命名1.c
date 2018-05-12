8ea043c8157ac3bc8756e0ca3e25d76f34aa7da7
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-05-12 10:57:32 +0800

    function

--- 未命名1.c
+++ 未命名1.c
@@ -12,6 +12,14 @@ main()
 		
 	while ((c = getchar()) != EOF)
 		if (c >= '0' && c <= '9')
-			++ndgit[c-'0'];
-		else if 
+			++ndigit[c-'0'];
+		else if (c == ' ' || c == '\n' || c == '\t')
+			++nwhite;
+		else
+			++nother;
+	printf("digits  =");
+	for (i = 0; i < 10; ++i)
+		printf(" %d", ndigit[i]);
+	printf(", white space = %d, other = %d\n",
+		nwhite, nother); 
  } 
