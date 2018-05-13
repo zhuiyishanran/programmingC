8941dce76de07828e9899fda2587563107f8eb61
zhuiyishanran <33598385+zhuiyishanran@users.noreply.github.com>
2018-05-13 16:48:47 +0800

    chapter3

---
+++ 未命名3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main()
+{
+	return 0;
+}
+/* ¶ş·Ö²éÕÒ */
+int binsearch(int x, int v[], int n)
+{
+	int low, high, mid;
+	
+	low = 0;
+	high = n - 1;
+	while (low <= high) {
+		mid = (low+high) / 2;
+		if (x < v[mid])
+			high = mid - 1;
+		else if (x > v[mid])
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return -1;
+ } 
